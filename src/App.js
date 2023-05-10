da3ad003ccae0df5e29a3e54d4da31f5f6ce976a
St3benn <anthonincolaspro@gmail.com>
2023-05-10 11:42:27 +0200

    [FEATURES]: adding Mantine Provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@mantine/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
